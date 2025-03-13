cf792100551b2aaf61477b151ba341f6d124ee9f
sdumax <maxdiogu@gmail.com>
2025-03-13 10:04:50 +0100

    improve SEO

--- app.vue
+++ app.vue
@@ -11,58 +11,67 @@
 </template>
 
 <script setup lang="ts">
-
-useHead({
-  title: 'Qodesquare Technologies',
-  meta: [
-    {
-      name: 'description',
-      content: 'Your website description here',
-    },
-    {
-      name: 'keywords',
-      content: 'Code, Tech, consulting, Nigeria, Best Tech consulting, Software Development, Web Development, Mobile App Development',  
-    },
-    {
-      name: 'author',
-      content: 'Qodesuare Technologies',
-    },
-    {
-      property: 'og:title',
-      content: 'Qodesquare Technologies',
-    },
-    {
-      property: 'og:description',
-      content: 'Your website description here',
-    },
-    {
-      property: 'og:type',
-      content: 'website',
-    },
-    {
-      property: 'og:url',
-      content: 'https://www.qodesquare.com',
-    },
-    {
-      property: 'og:image',
-      content: 'https://www.yourwebsite.com/image.jpg',
-    },
-    {
-      name: 'twitter:card',
-      content: 'summary_large_image',
-    },
-    {
-      name: 'twitter:title',
-      content: 'Qodesquare Technologies',
-    },
-    {
-      name: 'twitter:description',
-      content: 'Your website description here',
-    },
-    {
-      name: 'twitter:image',
-      content: 'https://www.yourwebsite.com/image.jpg',
-    },
-  ],
-});
+  useHead({
+    title: "Qodesquare Technologies",
+    meta: [
+      {
+        name: "robots",
+        content: "index, follow",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        name: "description",
+        content:
+          "Qodesquare specializes in software development, digital transformation, and IT consulting, helping businesses scale, optimize, and thrive. 🚀 Innovate. Elevate. Dominate.",
+      },
+      {
+        name: "keywords",
+        content:
+          "Code, Tech, consulting, Nigeria, Best Tech consulting, Software Development, Web Development, Mobile App Development",
+      },
+      {
+        name: "author",
+        content: "Qodesuare Technologies",
+      },
+      {
+        property: "og:title",
+        content: "Qodesquare Technologies",
+      },
+      {
+        property: "og:description",
+        content: "Your website description here",
+      },
+      {
+        property: "og:type",
+        content: "website",
+      },
+      {
+        property: "og:url",
+        content: "https://www.qodesquare.com",
+      },
+      {
+        property: "og:image",
+        content: "https://www.qodesquare.com/q_white_logo.png",
+      },
+      // {
+      //   name: 'twitter:card',
+      //   content: 'summary_large_image',
+      // },
+      // {
+      //   name: 'twitter:title',
+      //   content: 'Qodesquare Technologies',
+      // },
+      // {
+      //   name: 'twitter:description',
+      //   content: 'Your website description here',
+      // },
+      // {
+      //   name: 'twitter:image',
+      //   content: 'https://www.yourwebsite.com/image.jpg',
+      // },
+    ],
+  });
 </script>
