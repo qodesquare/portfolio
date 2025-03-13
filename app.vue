0c8d247aa9c8ba6768941dd5c4571b4525ca0c90
sdumax <maxdiogu@gmail.com>
2025-03-13 10:23:24 +0100

    improve SEO

--- app.vue
+++ app.vue
@@ -12,7 +12,7 @@
 
 <script setup lang="ts">
   useHead({
-    title: "Qodesquare Technologies",
+    title: "Qodesquare Technologies | Software Development, Digital Transformation, IT Consulting",
     meta: [
       {
         name: "robots",
